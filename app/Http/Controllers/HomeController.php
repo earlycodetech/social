38a76d68377c318100499b730cad4c18800b1cae
emma2625 <emmanuelodobo10@gmail.com>
2024-07-02 12:14:14 +0100

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = auth()->user();
+
+        if ($user->role == 'admin') {
+            return redirect()->route('feedback.show');
+        } else {
+            return redirect()->route('timeline.page');
+        }
+
+        dd($user->role);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TimelineController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,15 +14,26 @@ Route::get('/', function () {
 
 Route::get('give/feedback', [PagesController::class, "feedback"])->name('feedback.page');
 
-Route::get('show/feedbacks', [PagesController::class, "show_feedback"])->name('feedback.show');
 
 Route::post('give/feedback', [PagesController::class, "submit_feedback"])->name('feedback.submit');
 
-Route::patch('update/{id}/feedback', [PagesController::class, "update_feedback"])->name('feedback.update');
+/************* uri ************************  action  ******************** name *****/
 
-Route::delete('delete/{id}/feedback', [PagesController::class, "delete_feedback"])->name('feedback.delete');
-/************* uri ************************  action  ******************** name *****/ 
+Route::middleware(['auth', 'check.admin'])->group(function () {
+    Route::get('show/feedbacks', [PagesController::class, "show_feedback"])->name('feedback.show');
+    Route::patch('update/{id}/feedback', [PagesController::class, "update_feedback"])->name('feedback.update');
+    Route::delete('delete/{id}/feedback', [PagesController::class, "delete_feedback"])->name('feedback.delete');
+});
 
 Auth::routes();
 
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+
+Route::get('timeline', [TimelineController::class, 'index'])->middleware(['auth'])->name('timeline.page');
+Route::get('profile', [ProfileController::class, 'index'])->middleware(['auth'])->name('profile.page');
